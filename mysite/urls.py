a24913a415ecd5824e7a50482db97ed19c3724e1
Roman Veselý <rvesely88@gmail.com>
2025-03-04 15:33:01 +0100

    add quick start package

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
 
-from api import views
+from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
