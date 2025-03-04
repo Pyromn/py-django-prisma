63d36216fd86a1140a3d0c64d5d660ce29ede3c9
Roman Veselý <rvesely88@gmail.com>
2025-03-04 14:33:45 +0100

    test pydantic type adapter and class json schema

--- test_prisma.py
+++ test_prisma.py
@@ -1,8 +1,9 @@
 
 import asyncio
 
+from prisma.enums import Role
+from pydantic import PydanticUserError, TypeAdapter, ValidationError
 from prisma import Prisma
-
 from prisma.models import *
 
 async def main() -> None:
@@ -19,6 +20,41 @@ async def main() -> None:
 
     for post in posts:
         print(post)
+
+    try:
+        print(User.model_json_schema())
+    except PydanticUserError as e:
+        print(e)
+
+
+    user_list_adapter = TypeAdapter(List[User])
+    print(user_list_adapter.json_schema())
+
+    try:
+        user_list = user_list_adapter.validate_python([{
+            # 'id': '3',
+            'name': 'Fred',
+            'email': 'test@mail.com',
+            'role': Role.ADMIN,
+        }])
+        print(repr(user_list))
+    except ValidationError as e:
+        print(e)
+
+    # try without required email attribute
+    try:
+        user_list = user_list_adapter.validate_python([{
+            # 'id': '3',
+            'name': 'Fred',
+            # 'email': 'test@mail.com',
+            'role': Role.ADMIN,
+        }])
+        print(repr(user_list))
+    except ValidationError as e:
+        print(e)
+
+    print(repr(user_list_adapter.dump_json(user_list)))
+
 
     await db.disconnect()
 
